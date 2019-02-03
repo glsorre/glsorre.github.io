176676e28ee0dd11f0fefab3429c27f109f33557
Giuseppe Sorrentino <uhpnou@gmail.com>
2019-02-03 22:44:56 +0100

    improved 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -6,8 +7,13 @@ import SEO from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div class="home grid">
+      <div class="unit whole">
+        <h1>I am sorry...</h1>
+        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        <p>Please <Link to="/">check out</Link> this beautiful blog.</p>
+      </div>
+    </div>
   </Layout>
 )
 
