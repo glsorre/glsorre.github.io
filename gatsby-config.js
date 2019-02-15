bedd24a8cfbd1dd639b90b91b430697266d8747f
Giuseppe Lucio Sorrentino <gsorrentino@hotels.com>
2019-02-15 15:48:55 +0100

    changed google analytics options

--- gatsby-config.js
+++ gatsby-config.js
@@ -136,7 +136,7 @@ module.exports = {
         // Setting this parameter is optional
         anonymize: true,
         // Setting this parameter is also optional
-        respectDNT: true,
+        respectDNT: false,
         // Avoids sending pageview hits from custom paths
         exclude: [],
         // Enables Google Optimize using your container Id
