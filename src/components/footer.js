61fc90e20ead3bb16ebf92cadcaa3fa81559266d
Giuseppe Sorrentino <uhpnou@gmail.com>
2019-04-01 21:48:08 +0200

    improved footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,18 +4,20 @@ import { FaEnvelope, FaTwitter, FaGithub, FaLinkedin, FaRss } from 'react-icons/
 const Footer = () => (
   <footer class="footer clearfix" id="contacts">
     <div class="grid">
-      <div class="unit one-third">
+      <div class="unit one-quarter">
         <p class="author-copyright">
         &#169; {new Date().getFullYear()} rightright.me
         </p>
       </div>
-      <div class="unit two-thirds">
+      <div class="unit three-quarters">
         <p class="author-contacts">
-          <div><span><FaEnvelope /></span><span>giuseppe [at] rightright [dot] me</span></div> 
-          <a target="_blank" href="https://twitter.com/glsorre"><FaTwitter /></a> 
-          <a target="_blank" href="https://it.linkedin.com/in/glsorrentino"><FaLinkedin /></a> 
-          <a target="_blank" href="https://github.com/glsorre"><FaGithub /></a>
-          <a target="_blank" href="/feed.xml"><FaRss /></a> 
+          <div class="email"><span><FaEnvelope /></span><span>giuseppe [at] rightright [dot] me</span></div> 
+          <div class="others">
+            <a target="_blank" href="https://twitter.com/glsorre"><FaTwitter /></a> 
+            <a target="_blank" href="https://it.linkedin.com/in/glsorrentino"><FaLinkedin /></a> 
+            <a target="_blank" href="https://github.com/glsorre"><FaGithub /></a>
+            <a target="_blank" href="/feed.xml"><FaRss /></a>
+          </div>
         </p>
       </div>
     </div>
