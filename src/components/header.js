e4f86cf5b206e353aa44d1432991ff63a7bd3be0
Giuseppe Sorrentino <uhpnou@gmail.com>
2020-06-02 19:38:52 +0200

    fixed bug header in pages

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,8 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import sticker from "../../static/headerSticker_001.svg"
+
 const Header = ({ location }) => {
   return (
     <header class="header">
@@ -10,7 +12,7 @@ const Header = ({ location }) => {
           <div class="intro">
             <Link to="/"
                   state={{ filterLink: '🦄' }}>
-              <img class="sticker" alt="Hello, my name is Giuseppe Sorrentino and right|right is my personal brand." src="headerSticker_001.svg"/>
+              <img class="sticker" alt="Hello, my name is Giuseppe Sorrentino and right|right is my personal brand." src={sticker}/>
             </Link>
             <p>My professional objective is <b>to avoid failure of digital products</b> by thinking/managing them <b>right</b> (using ux and agile) and developing them <b>right</b>.</p>
           </div>
